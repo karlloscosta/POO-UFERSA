54c838311b2ebc63b7d4444ec10e78678767b4ca
karlloscosta <karllos.costa@alunos.ufersa.edu.br>
2026-03-16 22:03:43 -0300

    questao 11 atualizada

--- src/lista01questao11.java
+++ src/lista01questao11.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class lista01questao10 {
+public class lista01questao11 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
